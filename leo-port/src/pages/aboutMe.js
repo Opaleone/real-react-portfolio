3c6a1cdc057a7d08a10d2d522ff553186bd290f0
Leone Varando <106971202+Opaleone@users.noreply.github.com>
2022-12-17 20:25:06 -0600

    Added title and bio to about me

--- leo-port/src/pages/aboutMe.js
+++ leo-port/src/pages/aboutMe.js
@@ -1,3 +1,21 @@
 export const AboutMe = () => {
-  return
+  return (
+    <div id='about-me-container'>
+      <aside id='about-sidebar'>
+        <p id='about-title'>About Me</p>
+      </aside>
+      <div id='about-divider'></div>
+        <p id='bio'>Hello! My name is Leone Varando. I am a Full Stack Web developer who had experience creating
+        projects with HTML-CSS, JavaScript, React.JS, and much more.<br></br><br></br>I have many hobbies outside of my programming career as well. One such hobby is music production. I like to
+        create music
+        as well as listen to it regularly. Another hobby of mine is FPV drone flying. This is a hobby I have acquired
+        recently and
+        really enjoy as it makes me feel as though I am flying!<br></br><br></br>
+        I've began pursuing coding to jumpstart my career into something that gives me satisfaction in life as well as
+        give me the ability
+        to connect with others in a special way. I will get to create things for others, inspire others, and teach
+        others through a
+        medium that will quickly become something everyone will have to have a profiency in at some point.</p>
+    </div>
+  )
 }

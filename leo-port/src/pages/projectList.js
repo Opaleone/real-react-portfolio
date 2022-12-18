35eb26bd84c3b95477a30fdcc8e2bf7204b9cdd3
Leone Varando <106971202+Opaleone@users.noreply.github.com>
2022-12-17 20:26:54 -0600

    Added project info and began the addition of children to page

--- leo-port/src/pages/projectList.js
+++ leo-port/src/pages/projectList.js
@@ -1,3 +1,57 @@
+import { useState } from "react";
+
 export const Projects = () => {
-  return
+
+  const projectData = [
+    {
+      name: 'Redwood',
+      link: '',
+      image: '',
+      description: '',
+    },
+    {
+      name: 'Siege Bot',
+      link: '',
+      image: '',
+      description: '',
+    },
+    {
+      name: 'Weather Forecast',
+      link: '',
+      image: '',
+      description: '',
+    },
+    {
+      name: 'Eventify',
+      link: '',
+      image: '',
+      description: '',
+    },
+    {
+      name: 'DND Companion App',
+      link: '',
+      image: '',
+      description: '',
+    },
+    {
+      name: 'Something here',
+      link: '',
+      image: '',
+      description: '',
+    },
+  ]
+
+  return (
+    <div id='project-page'>
+      <h1>My Favorite Projects</h1>
+      <div id='project-container'>
+        {projectData.map((project, index) => (
+          <div className='project-card' key={index}>
+            <p>{project.name}</p>
+
+          </div>
+        ))}
+      </div>
+    </div>
+  )
 }

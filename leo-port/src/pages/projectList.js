551d8e3b5cc1086a88de0fb39dea29be1a732a63
Leone Varando <106971202+Opaleone@users.noreply.github.com>
2022-12-18 00:53:37 -0600

    Added logic and elements according to preferred styling

--- leo-port/src/pages/projectList.js
+++ leo-port/src/pages/projectList.js
@@ -1,4 +1,6 @@
 import { useState } from "react";
+import redwoodPic from '../assets/images/redwood-ss.png'
+import dndComp from '../assets/images/dnd-companion.png'
 
 export const Projects = () => {
 
@@ -6,49 +8,49 @@ export const Projects = () => {
     {
       name: 'Redwood',
       link: '',
-      image: '',
       description: '',
+      id: 'redwood'
     },
     {
       name: 'Siege Bot',
       link: '',
-      image: '',
       description: '',
+      id: 'siege-bot'
     },
     {
-      name: 'Weather Forecast',
+      name: 'J.A.T.E',
       link: '',
-      image: '',
       description: '',
+      id: 'jate'
     },
     {
       name: 'Eventify',
       link: '',
-      image: '',
-      description: '',
+      description: 'eventify',
+      id: 'eventify'
     },
     {
       name: 'DND Companion App',
       link: '',
-      image: '',
       description: '',
+      id: 'dnd-app'
     },
     {
       name: 'Something here',
       link: '',
-      image: '',
       description: '',
+      id: 'something'
     },
   ]
 
   return (
     <div id='project-page'>
-      <h1>My Favorite Projects</h1>
+      <h1 id='project-page-title'>My Favorite Projects</h1>
       <div id='project-container'>
         {projectData.map((project, index) => (
-          <div className='project-card' key={index}>
-            <p>{project.name}</p>
-
+          <div id={project.id} className='project-card'>
+            <p><span className='card-title'>{project.name}</span></p>
+            <div className='overlay'></div>
           </div>
         ))}
       </div>
